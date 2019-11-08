e983ce5090d195084c116b69149366fe5a68628d
Serhii <gespenst.serge@yandex.ru>
2019-11-08 09:04:28 +0200

    Updates

---
+++ precache-manifest.44e6ebab5902d7be3a2dc7cf054b0f60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d78eb39505aef825e925e8e457cf01b",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "5731972d8dc79f535914",
+    "url": "/react_tabs-with-router/static/css/main.6b171aae.chunk.css"
+  },
+  {
+    "revision": "7aa4ae05e8613e1b483b",
+    "url": "/react_tabs-with-router/static/js/2.3b62ef88.chunk.js"
+  },
+  {
+    "revision": "5731972d8dc79f535914",
+    "url": "/react_tabs-with-router/static/js/main.780c4fdf.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
